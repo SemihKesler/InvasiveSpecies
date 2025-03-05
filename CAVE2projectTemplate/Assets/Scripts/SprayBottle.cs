066eede5f973c7f31f3fb91db02785f22313fe47
pavankkannan <pavankkannan@gmail.com>
2025-03-05 09:24:35 -0600

    Spray Interaction
    On middle button mouse press, triclopyr will come out of the spray bottle's nozzle

---
+++ CAVE2projectTemplate/Assets/Scripts/SprayBottle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SprayBottle : MonoBehaviour
+{
+    public ParticleSystem sprayParticles;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            StartSpray();
+        }
+
+        if (Input.GetMouseButtonUp(2))
+        {
+            StopSpray();
+        }
+    }
+
+    void StartSpray()
+    {
+        sprayParticles.Play();
+    }
+
+    void StopSpray()
+    {
+        sprayParticles.Stop();
+    }
+}
