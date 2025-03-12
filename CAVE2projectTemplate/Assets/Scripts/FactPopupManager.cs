d4bae4600d75508c0e57ee49847ffc8a0fb98d15
Nefthali Arizmendi <nefthaliarizmendi@gmail.com>
2025-03-11 22:41:37 -0500

    Popup Fact Plants Issue
    I've done a script for when a person gets a plant, a popup shows up
    
    However, I'm having issues on how dragging the script will work on the invasive/Native letters

---
+++ CAVE2projectTemplate/Assets/Scripts/FactPopupManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // Make sure to include this for UI elements
+
+public class FactPopupManager : MonoBehaviour
+{
+    public Text popupText; // Reference to the Text component (inside your Canvas)
+
+    // Call this method when you want to show a fact
+    public void ShowFact(string fact)
+    {
+        popupText.text = fact; // Set the text message
+        popupText.gameObject.SetActive(true); // Show the text box
+        CancelInvoke(); // Stop any previous hide calls (so timer resets)
+        Invoke("HideFact", 4f); // Automatically hide after 4 seconds
+    }
+
+    // Method to hide the popup text
+    void HideFact()
+    {
+        popupText.gameObject.SetActive(false); // Hide the text box
+    }
+}
---
+++ CAVE2projectTemplate/Assets/Scripts/PlantPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantPickup : MonoBehaviour
+{
+    public FactPopupManager factManager; // Reference to the Fact Manager (you will assign this in the Inspector)
+    public string factMessage; // The message/fact to show when player picks up this plant
+
+    // This runs when the player touches the plant (make sure the plant has a Collider with "Is Trigger" checked!)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Check if it was the player
+        {
+            factManager.ShowFact(factMessage); // Show the fact using FactPopupManager
+            Destroy(gameObject); // Optional: remove the plant after it's picked up
+        }
+    }
+}
