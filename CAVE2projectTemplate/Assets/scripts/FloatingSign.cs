57246819d626532d2036c8c98514cfc4fc405e68
Nefthali Arizmendi <nefthaliarizmendi@gmail.com>
2025-03-17 19:08:35 -0500

    Instructions/Signposts
    I added the instructions and signposts.
    I make the instructions a popup script when the player enters the game.
    
    I also made some of the signposts float so the player can walk over to them to know what to do.

---
+++ CAVE2projectTemplate/Assets/scripts/FloatingSign.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingSign : MonoBehaviour
+{
+    public float floatSpeed = 1f; // Speed for it floating
+    public float floatHeight = 0.5f; // How high it moves up and down
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Make the sign float up and down
+        float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+}
---
+++ CAVE2projectTemplate/Assets/scripts/ShowInstruction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowInstruction : MonoBehaviour
+{
+    public TMP_Text instructionText;
+    public float displayTime = 10f; // How long to show instructions
+
+    void Start()
+    {
+        // Show the text when game starts
+        instructionText.gameObject.SetActive(true);
+
+        
+        Invoke("HideInstruction", displayTime);
+    }
+
+    void HideInstruction()
+    {
+        // Hide the text after seconds passes
+        instructionText.gameObject.SetActive(false);
+    }
+}
